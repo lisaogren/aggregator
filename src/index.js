ac92fc92e704256380ba529b137758c8cb72a9f2
RasCarlito <rascarlito@gmail.com>
2016-08-25 15:28:17 +0200

    feat(log): Add choo-log while in development

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ require('../static/theme/index.scss')
 // Init app
 const app = choo()
 
+// Plugins
+if (process.env.NODE_ENV !== 'production') {
+  const log = require('choo-log')
+  app.use(log())
+}
+
 // Init models
 const models = require('./models/index')
 models.map(model => app.model(model))
